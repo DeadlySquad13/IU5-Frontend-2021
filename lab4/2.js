d39cfd4bb0b93373363eafc81637c50875473911
DeadlySquad13 <sasha.pakalo@gmail.com>
2021-03-16 01:02:45 +0300

    2nd Completed

--- lab4/2.js
+++ lab4/2.js
@@ -6,8 +6,15 @@
  * "мир" -> false
  * "тот" -> true
  */
+/**
+ *
+ * @param {String} str
+ * @return {Boolean}
+ */
 function isPalindrome(str) {
-    //code here
+    const firstHalf = str.split('').splice(0, Math.floor(str.length / 2));
+    const secondHalf = str.split('').splice(Math.ceil(str.length / 2));
+    return firstHalf.join('') == secondHalf.reverse().join('');
 }
 
 module.exports = isPalindrome;
