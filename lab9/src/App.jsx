a2696c355fa69b7d5cf0efe408a18435d5e6222a
DeadlySquad13 <sasha.pakalo@gmail.com>
2021-04-26 00:20:54 +0300

    Regenerate api variable

--- lab9/src/App.jsx
+++ lab9/src/App.jsx
@@ -1,11 +1,5 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-  withRouter,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import SearchForm from './components/SearchForm/SearchForm.jsx';
 import Card from './components/Card/Card.jsx';
